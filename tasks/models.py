39e0c0df663f7430c0e9ffe9417663b3b1294261
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2022-05-03 21:59:29 +0400

    finish project tdoApp django and vue

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+#!Task Model
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True,null=True)
